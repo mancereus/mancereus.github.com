ce056818afebee03b53766482418b5329c7b416e
Matthias Schmitt <mschmitt@db12.de>
2016-03-27 11:28:05 +0200

    rain

--- monstermob/index.js
+++ monstermob/index.js
@@ -14850,8 +14850,8 @@ var cards = [
     {type:"Schatz",subtype:"B",count: 4, owner:""},
     {type:"Schatz",subtype:"C",count: 4, owner:""},
     {type:"Schatz",subtype:"D",count: 4, owner:""},
-    {type:"Herz",subtype:"A",count: 2, owner:""},
-    {type:"Herz",subtype:"B",count: 2, owner:""},
+    {type:"Schwert",subtype:"A",count: 2, owner:""},
+    {type:"Schwert",subtype:"B",count: 2, owner:""},
     {type:"Tür",subtype:"A",count: 2, owner:""},
     {type:"Tür",subtype:"B",count: 2, owner:""},
     {type:"Mob",subtype:"A",count: 4,dark:"dark", owner:""},
@@ -16928,6 +16928,172 @@ Polymer({
     @param {{node: Object}} detail Contains the animated node.
     */
   });
+Polymer({
+    is: 'iron-localstorage',
+
+    properties: {
+      /**
+       * localStorage item key
+       */
+      name: {
+        type: String,
+        value: ''
+      },
+      /**
+       * The data associated with this storage.
+       * If set to null item will be deleted.
+       * @type {*}
+       */
+      value: {
+        type: Object,
+        notify: true
+      },
+
+      /**
+       * If true: do not convert value to JSON on save/load
+       */
+      useRaw: {
+        type: Boolean,
+        value: false
+      },
+
+      /**
+       * Value will not be saved automatically if true. You'll have to do it manually with `save()`
+       */
+      autoSaveDisabled: {
+        type: Boolean,
+        value: false
+      },
+      /**
+       * Last error encountered while saving/loading items
+       */
+      errorMessage: {
+        type: String,
+        notify: true
+      },
+
+      /** True if value has been loaded */
+      _loaded: {
+        type: Boolean,
+        value: false
+      }
+    },
+
+    observers: [
+      '_debounceReload(name,useRaw)',
+      '_trySaveValue(autoSaveDisabled)',
+      '_trySaveValue(value.*)'
+    ],
+
+    ready: function() {
+      this._boundHandleStorage = this._handleStorage.bind(this);
+    },
+
+    attached: function() {
+      window.addEventListener('storage', this._boundHandleStorage);
+    },
+
+    detached: function() {
+      window.removeEventListener('storage', this._boundHandleStorage);
+    },
+
+    _handleStorage: function(ev) {
+      if (ev.key == this.name) {
+        this._load(true);
+      }
+    },
+
+    _trySaveValue: function() {
+      if (this._doNotSave) {
+        return;
+      }
+      if (this._loaded && !this.autoSaveDisabled) {
+        this.debounce('save', this.save);
+      }
+    },
+
+    _debounceReload: function() {
+      this.debounce('reload', this.reload);
+    },
+
+    /**
+     * Loads the value again. Use if you modify
+     * localStorage using DOM calls, and want to
+     * keep this element in sync.
+     */
+    reload: function() {
+      this._loaded = false;
+      this._load();
+    },
+
+    /**
+     * loads value from local storage
+     * @param {boolean=} externalChange true if loading changes from a different window
+     */
+    _load: function(externalChange) {
+      var v = window.localStorage.getItem(this.name);
+
+      if (v === null) {
+        this._loaded = true;
+        this._doNotSave = true;  // guard for save watchers
+        this.value = null;
+        this._doNotSave = false;
+        this.fire('iron-localstorage-load-empty', { externalChange: externalChange});
+      } else {
+        if (!this.useRaw) {
+          try { // parse value as JSON
+            v = JSON.parse(v);
+          } catch(x) {
+            this.errorMessage = "Could not parse local storage value";
+            console.error("could not parse local storage value", v);
+            v = null;
+          }
+        }
+        this._loaded = true;
+        this._doNotSave = true;
+        this.value = v;
+        this._doNotSave = false;
+        this.fire('iron-localstorage-load', { externalChange: externalChange});
+      }
+    },
+
+    /**
+     * Saves the value to localStorage. Call to save if autoSaveDisabled is set.
+     * If `value` is null or undefined, deletes localStorage.
+     */
+    save: function() {
+      var v = this.useRaw ? this.value : JSON.stringify(this.value);
+      try {
+        if (this.value === null || this.value === undefined) {
+          window.localStorage.removeItem(this.name);
+        } else {
+          window.localStorage.setItem(this.name, /** @type {string} */ (v));
+        }
+      }
+      catch(ex) {
+        // Happens in Safari incognito mode,
+        this.errorMessage = ex.message;
+        console.error("localStorage could not be saved. Safari incoginito mode?", ex);
+      }
+    }
+
+    /**
+     * Fired when value loads from localStorage.
+     *
+     * @event iron-localstorage-load
+     * @param {{externalChange:boolean}} detail -
+     *     externalChange: true if change occured in different window.
+     */
+
+    /**
+     * Fired when loaded value does not exist.
+     * Event handler can be used to initialize default value.
+     *
+     * @event iron-localstorage-load-empty
+     * @param {{externalChange:boolean}} detail -
+     *     externalChange: true if change occured in different window.
+     */
+  });
 (function() {
 
   Polymer({
@@ -17206,14 +17372,25 @@ Polymer({
                 notify: true,
                 value: function () { return []; }
             },
+            config: {type: Object,
+                value: function(){return {}}},
             activePlayer: {
                 type: String,
                 computed: 'isActive(container.active)'
+            },
+            myPlayer: {
+                type: Boolean,
+                computed: 'isMe(container)'
             }
 
         },
         isActive: function (active) {
             return active ? "am Zug" : "";
+        },
+        isMe: function (container) {
+            if (this.config.me == undefined)
+                this.config.me = '';
+            return this.config.me == container.name;
         },
         hasOwner: function (item) {
             return item.owner == this.container.name;
@@ -17267,6 +17444,7 @@ Polymer({
             is: 'monstermob-game',
             properties: {
                 game: Object,
+                config: Object,
                 players : {
                     type:Array,
                     notify: true,
@@ -17306,6 +17484,12 @@ Polymer({
                 if (this.game == undefined) {
                     this.newGame();
                 }
+            },
+             createConfig: function () {
+                 this.config = {
+                     me: {}
+                 }
+                this.notifyPath('config.me',this.config.me);
             },
             getMonsterIdx: function() {
                 return this.game.players.map(function(e) { return e.type; }).indexOf('monster');
